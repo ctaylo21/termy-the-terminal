d7631bde0498f03c682e85f300fc6372e7a4a4b3
Caleb Taylor <caleb.taylor@nike.com>
2019-01-15 16:40:11 -0800

    style(app.scss): add base dark theme

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Input({ handleKeyPress }) {
-  return <input type="text" onKeyPress={handleKeyPress} />;
+  return (
+    <div id="input-container">
+      <input type="text" onKeyPress={handleKeyPress} />
+    </div>
+  );
 }
 
 Input.propTypes = {
