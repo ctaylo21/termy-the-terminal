5ecf2204c028d9cedadaef5a2b4f379e1e05c0c2
Caleb Taylor <caleb89taylor@gmail.com>
2019-02-24 17:33:57 -0800

    feat(cd): add basic functionality in separate module

---
+++ src/services/cd.test.ts
@@ -0,0 +1,70 @@
+import cd, { FileSystem } from './cd';
+
+describe('cd suite', () => {
+  describe('valid cases', () => {
+    test('1 level path', () => {
+      const fileSystem: FileSystem = {
+        home: {
+          children: null,
+          type: 'FOLDER',
+        },
+      };
+
+      expect(cd(fileSystem, '/', 'home')).toBe(true);
+    });
+
+    test('multi-level cd', () => {
+      const fileSystem: FileSystem = {
+        home: {
+          children: {
+            folder1: {
+              type: 'FOLDER',
+              children: {
+                folder2: {
+                  type: 'FOLDER',
+                  children: null,
+                },
+              },
+            },
+          },
+          type: 'FOLDER',
+        },
+      };
+
+      expect(cd(fileSystem, '/', 'home/folder1/folder2')).toBe(true);
+    });
+  });
+
+  describe('invalid cases', () => {
+    test('empty path', () => {
+      const fileSystem: FileSystem = {
+        home: {
+          children: null,
+          type: 'FOLDER',
+        },
+      };
+      expect(cd(fileSystem, 'path', '')).toBe(false);
+    });
+
+    test('nested cd to a file', () => {
+      const fileSystem: FileSystem = {
+        home: {
+          children: {
+            folder1: {
+              type: 'FOLDER',
+              children: {
+                file1: {
+                  type: 'FILE',
+                  children: null,
+                },
+              },
+            },
+          },
+          type: 'FOLDER',
+        },
+      };
+
+      expect(cd(fileSystem, 'path', 'home/folder1/file1')).toBe(false);
+    });
+  });
+});
---
+++ src/services/cd.ts
@@ -0,0 +1,60 @@
+import { get, has } from 'lodash';
+
+export interface File {
+  type: 'FILE';
+  children: null;
+}
+
+export interface Folder {
+  type: 'FOLDER';
+  children?: FileSystem | null;
+}
+
+export interface FileSystem {
+  [key: string]: Folder | File;
+}
+
+/**
+ * Takes a valid Unix path and converts it into a format
+ * that matches the internal data structure. Doesn't handle
+ * leading "/" on path. Leaves ".." path piece alone.
+ *
+ * usr/home/test becomes usr/_children/_home/_children/test
+ * usr/home/../.. becomes usr/_children/_home/_children/../..
+ *
+ * @param {string} pathStr - path string to convert
+ * @returns {string} - converted string
+ */
+function convertPath(pathStr: string): string {
+  return pathStr
+    .split('/')
+    .map((elem, index, arr) => {
+      if (elem !== '..' && index !== arr.length - 1) {
+        elem += '.children';
+      }
+      return elem;
+    })
+    .join('.');
+}
+
+export default function cd(
+  fileSystem: FileSystem,
+  currentPath: string,
+  path: string,
+): boolean {
+  if (!path) {
+    return false;
+  }
+
+  const formattedPath = convertPath(path);
+
+  // Check if path exists in file system AND final item in path isn't a file
+  if (
+    has(fileSystem, formattedPath) &&
+    get(fileSystem, formattedPath).type !== 'FILE'
+  ) {
+    return true;
+  }
+
+  return false;
+}
